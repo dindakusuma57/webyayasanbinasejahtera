de19aaf67aab47a4af5fe15b4f0956717fc3a1a3
dindakusuma57 <dindakusuma@apps.ipb.ac.id>
2024-10-11 12:07:37 +0700

    add layout, navbar and footer

---
+++ resources/views/livewire/home-page.blade.php
@@ -0,0 +1,3 @@
+<div>
+    <h1 class="text-4xl text-red-500">Hello From Liveview 3</h1>
+</div>
--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Livewire\HomePage;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', HomePage::class);
 
 
